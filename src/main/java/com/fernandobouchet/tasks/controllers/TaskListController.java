0ef8f95bf353681f5c956eeb0109606547be6c11
fernandobouchet <fernandobouchet@gmail.com>
2025-01-27 23:52:58 -0300

    feat: add 'get task list' functionality

--- src/main/java/com/fernandobouchet/tasks/controllers/TaskListController.java
+++ src/main/java/com/fernandobouchet/tasks/controllers/TaskListController.java
@@ -7,6 +7,8 @@ import com.fernandobouchet.tasks.services.TaskListService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/task-lists")
@@ -33,5 +35,10 @@ public class TaskListController {
     public TaskListDto createTaskList (@RequestBody TaskListDto taskListDto) {
        TaskList createdTaskList =  taskListService.createTaskList(taskListMapper.fromDto(taskListDto));
        return taskListMapper.toDto(createdTaskList);
+    }
+
+    @GetMapping(path = "/{task_list_id}")
+    public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id")UUID taskListId){
+        return taskListService.getTaskList(taskListId).map(taskListMapper::toDto);
     }
 }
--- src/main/java/com/fernandobouchet/tasks/services/TaskListService.java
+++ src/main/java/com/fernandobouchet/tasks/services/TaskListService.java
@@ -3,8 +3,11 @@ package com.fernandobouchet.tasks.services;
 import com.fernandobouchet.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
