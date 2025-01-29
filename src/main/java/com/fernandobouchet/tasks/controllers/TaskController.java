51341c9ec78ea42c589cee10ea71092397a9a908
fernandobouchet <fernandobouchet@gmail.com>
2025-01-28 23:15:10 -0300

    feat: add 'create task' functionality

--- src/main/java/com/fernandobouchet/tasks/controllers/TaskController.java
+++ src/main/java/com/fernandobouchet/tasks/controllers/TaskController.java
@@ -1,12 +1,10 @@
 package com.fernandobouchet.tasks.controllers;
 
 import com.fernandobouchet.tasks.domain.dto.TaskDto;
+import com.fernandobouchet.tasks.domain.entities.Task;
 import com.fernandobouchet.tasks.mappers.TaskMapper;
 import com.fernandobouchet.tasks.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -30,5 +28,18 @@ public class TaskController {
                 .stream()
                 .map(taskMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskDto createTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @RequestBody TaskDto taskDto) {
+
+        Task createdTask = taskService.createTask(
+                taskListId,
+                taskMapper.fromDto(taskDto)
+        );
+
+        return taskMapper.toDto(createdTask);
     }
 }
--- src/main/java/com/fernandobouchet/tasks/services/impl/TaskServiceImpl.java
+++ src/main/java/com/fernandobouchet/tasks/services/impl/TaskServiceImpl.java
@@ -1,12 +1,17 @@
 package com.fernandobouchet.tasks.services.impl;
 
 import com.fernandobouchet.tasks.domain.entities.Task;
+import com.fernandobouchet.tasks.domain.entities.TaskList;
+import com.fernandobouchet.tasks.domain.entities.TaskPriority;
+import com.fernandobouchet.tasks.domain.entities.TaskStatus;
 import com.fernandobouchet.tasks.repositories.TaskListRepository;
 import com.fernandobouchet.tasks.repositories.TaskRepository;
 import com.fernandobouchet.tasks.services.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -26,4 +31,40 @@ public class TaskServiceImpl implements TaskService {
     public List<Task> listTasks(UUID taskListId) {
         return taskRepository.findByTaskListId(taskListId);
     }
+
+    @Override
+    public Task createTask(UUID taskListId, Task task) {
+        if (null != task.getId()) {
+            throw new IllegalArgumentException("Task already has an ID");
+        }
+
+        if (null != task.getTitle() || task.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Task must hava a title");
+        }
+
+        TaskPriority priority = Optional.ofNullable(task.getPriority())
+                .orElse(TaskPriority.MEDIUM);
+
+        TaskList taskList = taskListRepository
+                .findById(taskListId)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Invalid Task List ID provided"));
+
+        LocalDateTime now = LocalDateTime.now();
+
+        return taskRepository.save(
+                new Task(
+                        null,
+                        task.getTitle(),
+                        task.getDescription(),
+                        task.getDueDate(),
+                        TaskStatus.OPEN,
+                        priority,
+                        taskList,
+                        now,
+                        now
+                ));
+
+    }
+
 }
